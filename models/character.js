d2906cd25fc3e2223d90636a7762f8ee9fc6f7a1
Jeff Magill <jeffmagill@outlook.com>
2016-10-15 00:05:06 -0700

    improved player animation

--- models/character.js
+++ models/character.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 module.exports = function(sequelize, DataTypes) {
-  var Character = sequelize.define('Character', {
-    name: DataTypes.STRING,
-    picture: DataTypes.STRING,
-    description: DataTypes.STRING,
-    hp: DataTypes.INTEGER,
-    ap: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
+    var Character = sequelize.define("Character", {
+        name: DataTypes.STRING,
+        picture: DataTypes.STRING,
+        description: DataTypes.STRING,
+        hp: DataTypes.INTEGER,
+        ap: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function(models) {
         // associations can be defined here
-      }
-    }
-  });
-  return Character;
+            }
+        }
+    });
+    return Character;
 };
