398dd57487188dbc9ca7c81caef307e5a82a351d
Arpad Asztalos <arpad.web@gmail.com>
2016-10-18 17:20:55 -0700

    fit canvas to window, displayed player health / attack points

--- public/assets/scripts/game.js
+++ public/assets/scripts/game.js
@@ -1,5 +1,5 @@
 //test comment for commit
-var game = new Phaser.Game( 800, 600, Phaser.AUTO, "", {
+var game = new Phaser.Game( window.innerWidth, window.innerHeight, Phaser.AUTO, "", {
     preload: preload,
     create: create,
     update: update,
@@ -465,4 +465,6 @@ function render() {
     //TODO: put the player's x/y coordinates on the screen, this same code can be used to get the player's coordinates to save to the database
     game.debug.text( 'Tile X: ' + grassLayer.getTileX( player.x ), 32, 48, textColor );
     game.debug.text( 'Tile Y: ' + grassLayer.getTileY( player.y ), 32, 64, textColor );
+    game.debug.text( 'Health: ' + player.hp, 232, 48, textColor );
+    game.debug.text( 'Health: ' + player.ap, 432, 48, textColor );
 }
